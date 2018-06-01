0c5d551902a5f351df4cb71032274f6939fc6ec0
David van Zanten <dmtvanzanten@gmail.com>
2018-06-01 22:06:30 +0200

    Added file structure...

---
+++ T10Modules/ui_elements.py
@@ -0,0 +1,7 @@
+""" 
+ui_elements.py
+Contains stand-alone classes and functions that are convenient to use with plots 
+generated with MPL. Examples are: dynamics cross-section tools, ROI, etc.
+
+Author: David van Zanten
+"""
